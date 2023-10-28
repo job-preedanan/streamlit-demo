a08eaa08dd1b4c49c75ee99b1879b43fc2ad53da
job-preedanan <wongsakorn.pre@axonstech.com>
2023-10-28 13:55:06 +0700

    update 3-cls model path

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -112,7 +112,7 @@ def main():
     # model_url = 'https://drive.google.com/uc?export=download&id=1o14U3yNxIBQPU5dD86IPjs8o5FffuFfw' # old 518
     model_url_3cls = 'https://drive.google.com/uc?export=download&id=1_8gX7MKa02i7sIgjJ1pJEuGzpfouHPl-'
     model_url_4cls = 'https://drive.google.com/uc?export=download&id=16fLZFDg7_lrMdYV57GuzL78IWoAHpVKl'   # 336 multi-label
-    model_path_3cls = 'DINOb(f)_3cls_336_0_best.pt'
+    model_path_3cls = 'DINOb(f)_3cls_336_2_best.pt'
     model_path_4cls = 'DINOb(f)_4cls_336_1_best.pt'
     if num_classes == 3:
         download_weights(model_url_3cls, model_path_3cls)
