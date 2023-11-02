0cd33664bc87dfb0d37cef77092b00bc60ef28cd
job-preedanan <wongsakorn.pre@axonstech.com>
2023-11-02 13:04:15 +0700

    edit post logic2

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -98,7 +98,7 @@ def classify_image(image, model, image_size, num_classes=4):
         probabilities = torch.sigmoid(output[0])
         # predicted_class = (probabilities.data > 0.5).float()      # multi outputs
         predicted = torch.zeros_like(probabilities)
-        for i in range(len(outputs)):
+        for i in range(len(probabilities)):
             predicted[i] = post_process(probabilities[i], 0.5)            # post processing
 
     elif num_classes == 3:
